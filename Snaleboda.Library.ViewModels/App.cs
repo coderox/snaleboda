83b9bb3e21cbfb10817ab3e64f06d88a4760ac8e
Johan Lindfors <Johan.Lindfors@coderox.se>
2014-09-09 21:12:13 +0200

    Refactored the library

---
+++ Snaleboda.Library.ViewModels/App.cs
@@ -0,0 +1,22 @@
+using Cirrious.CrossCore;
+using Cirrious.CrossCore.IoC;
+using Snaleboda.Library.Interfaces;
+using Snaleboda.Library.Utilities;
+using Snaleboda.Library.Services;
+
+namespace Snaleboda.Library
+{
+    public class App : Cirrious.MvvmCross.ViewModels.MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Agent")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            Mvx.RegisterType<IAsyncServiceAgent, AsyncServiceAgent>();
+            RegisterAppStart<ViewModels.MainViewModel>();
+        }
+    }
+}
