6eaad22a547b90c255310c5e786670b8beb4fb61
Daniel Hindrikes <daniel@hindrikes.se>
2014-09-16 18:56:19 +0200

    views

--- Xamarin.Forms/Snaleboda.XamarinForms/Views/MainView.xaml.cs
+++ Xamarin.Forms/Snaleboda.XamarinForms/Views/MainView.xaml.cs
@@ -9,9 +9,12 @@ namespace Snaleboda.XamarinForms.Views
 {
     public partial class MainView
     {
-        public MainView(NewsViewModel viewModel)
+        public MainView(NewsViewModel viewModel, NewsView newsView, ContactsView contactsView, IncidentView incidentView)
         {
             InitializeComponent();
+            Children.Add(newsView);
+            Children.Add(contactsView);
+            Children.Add(incidentView);
         }
     }
 }
