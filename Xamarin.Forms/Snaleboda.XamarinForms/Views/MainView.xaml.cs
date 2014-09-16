25d8f051efccddf24553fbd0644ff2780f23ec5c
johan.kson@gmail.com <johan.kson@gmail.com>
2014-09-16 18:41:33 +0200

    added mainview

---
+++ Xamarin.Forms/Snaleboda.XamarinForms/Views/MainView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Snaleboda.XamarinForms.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snaleboda.XamarinForms.Views
+{
+    public partial class MainView
+    {
+        public MainView(NewsViewModel viewModel)
+        {
+            InitializeComponent();
+        }
+    }
+}
