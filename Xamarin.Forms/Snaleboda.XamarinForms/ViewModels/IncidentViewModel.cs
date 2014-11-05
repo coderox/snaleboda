c290ec3544d98f82189127f866a2de6c25060428
Daniel Hindrikes <daniel@hindrikes.se>
2014-11-05 15:59:19 +0100

    början på incident

--- Xamarin.Forms/Snaleboda.XamarinForms/ViewModels/IncidentViewModel.cs
+++ Xamarin.Forms/Snaleboda.XamarinForms/ViewModels/IncidentViewModel.cs
@@ -1,12 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Snaleboda.XamarinForms.ViewModels
 {
-    public class IncidentViewModel
+    public class IncidentViewModel : INotifyPropertyChanged
     {
+        private bool _showPhoto;
+        public bool ShowPhoto
+        {
+            get
+            {
+                return _showPhoto;
+            }
+            set
+            {
+                _showPhoto = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _showButtons;
+        public bool ShowButtons
+        {
+            get
+            {
+                return _showButtons;
+            }
+            set
+            {
+                _showButtons = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand TakePhoto
+        {
+            get
+            {
+                return new Command(() =>
+                {
+
+                });
+            }
+        }
+
+        public ICommand SelectPhoto
+        {
+            get
+            {
+                return new Command(() =>
+                {
+
+                });
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
