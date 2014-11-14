6ad87fccbb5e6d8afc8774a4a163ecbabfab2247
Andreas Hammar <andreas.hammar@jayway.com>
2014-11-14 01:30:55 +0100

    added some sample data

---
+++ Konverterad/Snaleboda.Universal/Snaleboda.Universal.Shared/ViewModels/MainPageVm.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Web.Syndication;
+
+namespace Snaleboda.Universal.ViewModels
+{
+    public class MainPageVm : VmBase
+    {
+        public IList<NewsVmi> NewsItems { get; set; }
+        public IList<ContactVmi> ContactItems { get; set; }
+
+        public MainPageVm()
+        {
+        }
+
+        public async Task InitAsync()
+        {
+        }
+    }
+}
